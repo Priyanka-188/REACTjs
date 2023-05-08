e592e88d7a40ae66f9a95659d248340fbdd65eb9
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-05-08 13:02:30 +0530

    second commit

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name = "Hare Krsna"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I am Learning React.
-        </a>
-      </header>
-    </div>
+   <>
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+   </nav>
+   <div className="container">
+    <h1>Chant {name}</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate id maxime doloremque facere deleniti reprehenderit accusantium provident pariatur necessitatibus recusandae architecto quos labore modi, et beatae? Pariatur nulla quo accusantium eius provident dicta corporis praesentium.
+   </div>
+   </>
   );
 }
 
