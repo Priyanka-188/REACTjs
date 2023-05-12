46675c8b7d970b03990cb9e9c78067b9958c4529
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-05-12 16:03:04 +0530

    Alerts video 13

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,19 +4,34 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
+import Alert from "./components/Alert";
 // import AboutUs from "./components/AboutUs";
 
 function App() {
   // make a state to see wheather mode will be dark or light 
   const[mode,setMode] = useState('light');
+  const[alert,setAlert]= useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  
   const toggleMode =()=>{
     if (mode ==='light'){
       setMode('dark');
       document.body.style.backgroundColor ='#052c65';
+      showAlert("Dark mode has been enabled","success ");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor ='white';
+      showAlert("Light mode has been enabled","success ");
     }
   }
   return (
@@ -24,8 +39,10 @@ function App() {
       <Navbar title="TextFun" about="About" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar/> */}
 
+      <Alert alert = {alert}/>
+
       <div className="container my-3">
-        <TextForm heading="Enter the Text to analyze" mode={mode}/>
+        <TextForm heading="Enter the Text to analyze" mode={mode} showAlert ={showAlert}/>
       </div>
 
       {/* <AboutUs/> */}
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -6,23 +6,21 @@ export default function TextForm(props) {
     // console.log("Uppercase was clicked!" +text)
     let newtext = text.toUpperCase();
     setText(newtext);
+    props.showAlert("Converted to upperCase! ","success");
   };
 
   // arrow fucntion to change text to lowercase
   const handleLowClick = () => {
     let newtext = text.toLowerCase();
     setText(newtext);
+    props.showAlert("Converted to lowerCase! ","success");
   };
   // function to clear whole text
   const handleclear = () => {
     setText(" ");
+    props.showAlert("Text cleared ! ","success");
   };
 
-  //function to capitalize the text
-  const handleSentenceCase = () => {
-    const newtext = text.charAt(0).toUpperCase() + text.slice(1);
-    setText(newtext);
-  };
 
   //function to capitalize first letter of every word
   const handleCapitalize = () => {
@@ -33,6 +31,7 @@ export default function TextForm(props) {
     }
     const newtext = arr.join(" ");
     setText(newtext);
+    props.showAlert("Capitalize the text ! ","success");
   };
 
   //function to capitalize first letter of every line.
@@ -44,7 +43,8 @@ export default function TextForm(props) {
       arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
     const newtext = arr.join(". ");
-    setText(newtext);
+    setText(newtext); 
+    props.showAlert("Capitalize first letter of each word ! ","success");
   };
 
   //  function to copy the text to the clipboard
@@ -53,25 +53,30 @@ export default function TextForm(props) {
     arr = arr.reverse();
     const newtext = arr.join(" ");
     setText(newtext);
+    props.showAlert("Text is Reversed ! ","success");
   }
 
-  // arrow function to handle additon of text in the textare
-  const handleOnChange = (event) => {
-    // console.log("handle on change");
-    setText(event.target.value);
-  };
-
+  
   //function to copy the text on clipboard
   const handleCopy =()=>{
     var text = document.getElementById("mybox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to clipboard ! ","success");
   }
 
   const RemoveExtraSpaces= ()=>{
     let newtext = text.split(/[  ]+/);
     setText(newtext.join(" "))
+    props.showAlert("Extra-Spaces Removed ! ","success");
   }
+
+  // arrow function to handle additon of text in the textarea
+  const handleOnChange = (event) => {
+    // console.log("handle on change");
+    setText(event.target.value);
+  };
+
 
   // Creating a state to change to text to uppercase lowercase or some other operation.
   const [text, setText] = useState(" ");
@@ -86,8 +91,8 @@ export default function TextForm(props) {
           className="form-control "
           id="mybox"
           rows="8"
-          value={text}
           style={{backgroundColor:props.mode==="dark"? 'grey':'white',color:props.mode==="dark"? 'white':'black'}}
+          value={text}
           onChange={handleOnChange} //setting the value of text default initially but change in text (addition of text ) will be handle by onChnage (handleOnClick ) function.
         ></textarea>
         <button className="btn btn-primary my-3" onClick={handleUpClick}>
@@ -98,24 +103,21 @@ export default function TextForm(props) {
           {" "}
           Lowercase
         </button>
-        <button className="btn btn-primary my-3  mx-1" onClick={handleSentenceCase}>
-          SentenceCase
-        </button>
         <button className="btn btn-primary  my-3 mx-1" onClick={handleCapitalize}>
-          CapitalizeCase
+          Capitalize Case
         </button>
         <button className="btn btn-primary my-3  mx-1" onClick={handleFirstLetter}>
-          CapitalizeFirstLetter
+          Capitalize After FullStop
         </button>
         <button className="btn btn-primary my-3  mx-1" onClick={handleReverseString}>
-          ReverseText
+          Reverse Text
         </button>
        
         <button className="btn btn-primary my-3  mx-1" onClick={handleCopy}>
           Copy
         </button>
         <button className="btn btn-primary  my-3 mx-1" onClick={RemoveExtraSpaces}>
-          RemoveExtraSpaces
+          Remove ExtraSpaces
         </button>
 
         <button className="btn btn-primary  my-3 mx-1" onClick={handleclear}>
